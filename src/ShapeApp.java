cd89f16c616f9bfab080c6f12891f34145407347
mybear517 <mybear517@gmail.com>
2021-02-03 23:05:13 -0500

    modified:   src/ShapeApp.java 	modified:   src/Square.java 	modified:   src/Triangle.java

--- src/ShapeApp.java
+++ src/ShapeApp.java
@@ -2,33 +2,33 @@
 public class ShapeApp {
 
 	public static void main(String[] args) {
-       // create shapes
+		// create shapes
 		Shape shape = null;
-		
+
 		// set choice
 		// if 1 set circle > set radius
 		//    2 set square > set side size
 		//    3 set triangle > set height & base
 		int choice = 1;
-		
+
 		int radious = 6;
 		int side = 7;
 		int height = 4;
 		int base = 10;
-		
-     if(choice == 1) {
-    	 shape = new Circle(radious);
-     } else if(choice == 2) {
-    	 shape = new Square(side);
-     } else if(choice == 3) {
-    	 shape = new Triangle(height,base);
-     } else {
-    	 System.out.println("Invalid Choice > " + choice);
-    	 shape = new Shape();
-     }
-     
-     double area = shape.calculateArea();
-     System.out.println("Calculate Area -> " + area);
+
+		if(choice == 1) {
+			shape = new Circle(radious);
+		} else if(choice == 2) {
+			shape = new Square(side);
+		} else if(choice == 3) {
+			shape = new Triangle(height,base);
+		} else {
+			System.out.println("Invalid Choice > " + choice);
+			shape = new Shape();
+		}
+
+		double area = shape.calculateArea();
+		System.out.println("Calculate Area -> " + area);
 
 	}
 
--- src/Square.java
+++ src/Square.java
@@ -1,20 +1,20 @@
 
 public class Square extends Shape {
-    private int size;
+	private int size;
 	public Square() {
 		size = 0;
 	}
-    public Square(int side) {
-    	this.size = side;
-    }
-    public int getSide() {
-    	return size;
-    }
-    public void setSide(int size) {
-    	this.size = size;
-    }
- public double calculateArea() {
-	 double area = size * size;
-	 return area;
-    }
+	public Square(int side) {
+		this.size = side;
+	}
+	public int getSide() {
+		return size;
+	}
+	public void setSide(int size) {
+		this.size = size;
+	}
+	public double calculateArea() {
+		double area = size * size;
+		return area;
+	}
 }
